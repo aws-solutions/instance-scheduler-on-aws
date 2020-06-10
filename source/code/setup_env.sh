5ac5c65c5a7fe2d88958bb8fe524a6fae1e30919
Florian Kauschke <florian.kauschke@trustedshops.de>
2020-06-10 13:34:27 +0200

    remove unnecessary files and lines for pull request

--- source/code/setup_env.sh
+++
@@ -1,9 +0,0 @@
-pip install -r requirements.txt
-[ -d ./tmp/pytz ] && rm -r ./tmp/pytz
-[ -d ./tmp/requests ] && rm -r ./tmp/requests
-pip install pytz -t ./tmp/pytz && pip install requests -t ./tmp/requests
-for pkg in certifi chardet idna requests urllib3; do
-    [ ! -d ./$pkg ] && mv ./tmp/requests/$pkg ./$pkg
-done
-[ ! -d ./pytz ] && mv ./tmp/pytz ./pytz
-rm -r ./tmp
