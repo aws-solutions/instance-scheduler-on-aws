ef06e623cec281ea0531a8c91b1a15935f08b8d8
David Latham <david.latham@amp.co.nz>
2022-03-15 09:44:45 +1300

    Arn for license-configuration ends in ":*" not "/*"

--- source/lib/aws-instance-scheduler-remote-stack.ts
+++ source/lib/aws-instance-scheduler-remote-stack.ts
@@ -105,7 +105,7 @@ export class AwsInstanceSchedulerRemoteStack extends cdk.Stack {
                             effect: Effect.ALLOW,
                             resources: [
                                 cdk.Fn.sub("arn:${AWS::Partition}:ec2:*:${AWS::AccountId}:instance/*"),
-                                cdk.Fn.sub("arn:${AWS::Partition}:license-manager:*:${AWS::AccountId}:license-configuration/*")
+                                cdk.Fn.sub("arn:${AWS::Partition}:license-manager:*:${AWS::AccountId}:license-configuration:*")
                             ]
                         }),
                         new PolicyStatement({
