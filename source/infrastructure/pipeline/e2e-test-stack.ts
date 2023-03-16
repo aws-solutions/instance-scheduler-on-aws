6d88e635c9f12812230e304b5f06059019cb55db
Caleb Pearson <calepea@amazon.com>
2023-03-16 16:37:16 -0400

    Create EC2 test instance

--- source/infrastructure/pipeline/e2e-test-stack.ts
+++ source/infrastructure/pipeline/e2e-test-stack.ts
@@ -1,7 +1,7 @@
 
 import * as cdk from "aws-cdk-lib"
 import {Construct} from "constructs";
-
+import {BasicEc2StartStopTest} from "./e2e-tests/basic-ec2-start-stop.test";
 export interface E2ETest {
   createTestResources(scope: Construct) : void
   runTests() : void
@@ -11,14 +11,13 @@ export class E2eTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    for(const e2eTest of listE2ETests()) {
+    for (const e2eTest of listE2ETests()) {
       e2eTest.createTestResources(this);
     }
 
     cdk.Stack.of(this);
   }
 }
-
 function listE2ETests() : E2ETest[] {
-  return [];
+  return [new BasicEc2StartStopTest];
 }
