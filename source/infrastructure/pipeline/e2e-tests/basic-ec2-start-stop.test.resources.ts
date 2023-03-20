b4396ddc4b0dca79c5177173604bf9f1a0c1f027
Caleb Pearson <calepea@amazon.com>
2023-03-20 12:52:29 -0400

    pass EC2 Instance id as env variable

--- source/infrastructure/pipeline/e2e-tests/basic-ec2-start-stop.test.resources.ts
+++ source/infrastructure/pipeline/e2e-tests/basic-ec2-start-stop.test.resources.ts
@@ -4,6 +4,9 @@ import * as cdk from "aws-cdk-lib";
 import {NagSuppressions} from "cdk-nag";
 import {TestResourceProvider} from "./index";
 
+
+export const START_STOP_TEST_SCHEDULE_NAME = 'basic-start-stop-test-schedule'
+export const START_STOP_TEST_INSTANCE_ID_OUT_PATH = 'basic-start-stop-instance-id'
 export class EC2StartStopTestResources implements TestResourceProvider {
   createTestResources(scope: Construct) {
 
@@ -11,14 +14,17 @@ export class EC2StartStopTestResources implements TestResourceProvider {
       ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16")
     })
 
-    const testInstance = new ec2.Instance(scope, "basic-start-stop-instance-MODIFIED", {
-      instanceName: "basic-start-stop-instance-MODIFIED",
+    const testInstance = new ec2.Instance(scope, "basic-start-stop-instance", {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
       machineImage: ec2.MachineImage.latestAmazonLinux(),
       vpc: vpc
     })
 
-    cdk.Tags.of(testInstance).add("Schedule", "basic-start-stop-test-schedule")
+    cdk.Tags.of(testInstance).add("Schedule", START_STOP_TEST_SCHEDULE_NAME)
+
+    const startStopOut = new cdk.CfnOutput(scope, "SOSSO", {
+      value: testInstance.instanceId,
+    })
 
     NagSuppressions.addResourceSuppressions(testInstance, [
       {
@@ -37,6 +43,17 @@ export class EC2StartStopTestResources implements TestResourceProvider {
         reason: "testing resource"
       },
     ])
+
+    return {
+      "SV_StartStop": startStopOut,
+      "SV_VPC": new cdk.CfnOutput(scope, "SV_VPC", {
+        value: vpc.vpcArn
+      }),
+      "SV_FixedTest": new cdk.CfnOutput(scope, "SV_FixedTest", {
+        value: "searchable-value1",
+        exportName: "exportName"
+      })
+    }
   }
 
 }
--- source/infrastructure/pipeline/lib/e2e-test-stack.ts
+++ source/infrastructure/pipeline/lib/e2e-test-stack.ts
@@ -2,17 +2,17 @@
 import * as cdk from "aws-cdk-lib"
 import {Construct} from "constructs";
 import {testResourceProviders} from "../e2e-tests";
-export interface E2ETest {
-  createTestResources(scope: Construct) : void
-  runTests() : void
-}
+import {CfnOutput} from "aws-cdk-lib";
 
 export class E2eTestStack extends cdk.Stack {
+
+  outputs: Record<string, CfnOutput> = {}
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     for (const testResourceProvider of testResourceProviders) {
-      testResourceProvider.createTestResources(this);
+      let output = testResourceProvider.createTestResources(this);
+      this.outputs = {...this.outputs, ...output}
     }
 
     cdk.Stack.of(this);
