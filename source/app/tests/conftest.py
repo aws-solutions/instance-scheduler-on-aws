9d2ce5d48e9e9323a283845dab70337f09fb4a22
Caleb Pearson <calepea@amazon.com>
2023-04-14 10:14:46 -0400

    replace copyright headers

--- source/app/tests/conftest.py
+++ source/app/tests/conftest.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from unittest import mock
 import os
 import pytest
--- source/cli/tests/test_version.py
+++ source/cli/tests/test_version.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from instance_scheduler_cli import __version__
 
 
